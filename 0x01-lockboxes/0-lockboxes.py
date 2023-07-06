ec7844c12c1226d7adabefc3480a70c307d20795
eregbene <benedicteregbene@gmail.com>
2023-07-06 18:32:27 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
+"""
+Task 0: Lockboxes
+"""
 
 
 def canUnlockAll(boxes):
-    """Unlock array of boxes of keys with indices"""
-    size = len(boxes)
-    checker = {}
-    index = 0
+    """
+    Determines if all boxes can be opened
+    """
+    if boxes is None or len(boxes) is 0:
+        return False
 
-    for keys in boxes:
-        if len(keys) == 0 or index == 0:
-            checker[index] = -1  # -1 means box is empty
-        for key in keys:
-                if key < size and key != index:
-                    checker[key] = key
-        if len(checker) == size:
-            return True
-        index += 1
-    return False
+    status = ["T"]
+
+    for box in range(1, len(boxes)):
+        status.append("F")
+
+    for box in range(0, len(boxes)):
+        if (status[box] is "T" or box is 0):
+            for key in boxes[box]:
+                if int(key) < len(boxes) and status[key] is "F":
+                    for k in boxes[key]:
+                        if k < len(boxes):
+                            status[k] = "T"
+                if key < len(boxes):
+                    status[key] = "T"
+
+    if "F" in status:
+        return False
+    return True
